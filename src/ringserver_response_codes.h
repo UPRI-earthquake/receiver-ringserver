415331980904b9095a0bdaa3150dea8acc1bb5c6
prokorpio@hp <sanchez.christopherjeff@gmail.com>
2023-06-21 14:33:35 +0800

    feat: create ringserver responses and rename file for authserver responses

---
+++ src/ringserver_response_codes.h
@@ -0,0 +1,17 @@
+#ifndef RINGSERVER_RESPONSE_CODES_H
+#define RINGSERVER_RESPONSE_CODES_H
+
+// Define constant integer values for response status codes
+#define GENERIC_SUCCESS                 0
+#define GENERIC_ERROR                   1
+
+// Define response codes as strings
+#define GENERIC_SUCCESS_STR                 "GENERIC_SUCCESS"
+#define GENERIC_ERROR_STR                   "GENERIC_ERROR"
+
+// Define the default messages for each response status code
+#define GENERIC_SUCCESS_MSG                 "Success"
+#define GENERIC_ERROR_MSG                   "Error"
+
+
+#endif  /* RINGSERVER_RESPONSE_CODES_H */
